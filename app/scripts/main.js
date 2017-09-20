12d74a551fc4e9b1cdf14b1509a6618ac2de3c5f
katherineortega <katy_ja2915@hotmail.com>
2017-09-19 22:06:57 -0500

    mostrar imagenes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,9 @@
-import './plugin';
+import mostrarPines from './plugin';
 import data from './data.js';
 
 $(document).ready(function () {
   // $("#imagenes").pinterestify({
   //   data
   // });
+  mostrarPines(data);
 });
