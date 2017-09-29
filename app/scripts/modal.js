44c6312e79390014e3677458ac0d8f747e4bf11e
maryleo3007 <maryleo.3007@gmail.com>
2017-09-28 22:29:53 -0500

    pinterestify ok

--- app/scripts/modal.js
+++ app/scripts/modal.js
@@ -7,13 +7,14 @@ const mostrarModal = (contenedorPines,pines) =>{
     console.log(id);
 
     const [pin] = pines.filter(pin => pin.id == id);
-    const {title, description, user, hashtag} = pin;
+    const {title, description, user, hashtag, image_url} = pin;
 
     modal.removeClass('inactivo');
-    modal.find('h4').text(title);
-    modal.find('p').text(description);
-    modal.find('.user').text(user);
-    modal.find('.hashtag').text(hashtag);
+    modal.find('.post--image').attr('src','images/'+image_url);
+    modal.find('.post--title').text(title);
+    modal.find('.post--description').text(description);
+    modal.find('.post--user').text(user);
+    modal.find('.post--hashtag').text(hashtag);
     modal.on('click',function () {
       modal.addClass('inactivo');
     });
