79516f57909c014c3b4bc0c2ce11c24fae73f00b
Mitchell Rodríguez <mitrodle@gmail.com>
2017-09-29 01:34:30 -0500

    Print pins + Modal | Pinterestify status: Finished

--- app/scripts/modal-pines.js
+++ app/scripts/modal-pines.js
@@ -1,6 +1,25 @@
 const modalPines = (pinesContainer, pines) => {
 
-	pinesContainer.on('click', 'post', function() {
-		// const id = $(this).find()
-	})
-}
+	pinesContainer.on('click', '.post', function() {
+		const id = $(this).attr('id');
+		const modal = $('.modal');
+		const [pin] = pines.filter(pin => pin.id == id);
+
+		modal.removeClass('inactivo');
+
+		const {title, description, image_url, user, username, hashtag} = pin;
+
+		modal.find('.post--title').text(title);
+		modal.find('.post--image').attr('src', `images/${image_url}`);
+		modal.find('.post--username').text(`${(pin.username).charAt(0).toUpperCase()}`);
+		modal.find('.post--user').text(user);
+		modal.find('.post--hashtag').text(`#${hashtag}`);
+		modal.find('.post--description').text(description);
+	});
+
+	$('.close').on('click', function() {
+		$('.modal').addClass('inactivo');
+	});
+};
+
+export default modalPines;
