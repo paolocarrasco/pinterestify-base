3ac287e0538f68686f4af945479b06ef0e4426d7
Annia <itsandromeda@gmail.com>
2017-09-28 23:34:41 -0500

    Displaying modal

---
+++ app/scripts/mostrarmodal.js
@@ -0,0 +1,33 @@
+const showmodal = (modalContainer, pin) => {
+
+    modalContainer.on('click', '.post', function () {
+        const id = $(this).attr('id');
+        const modal = $('.modal');
+        console.log(id);
+        const [post] = pin.filter(post => post.id == id);
+
+        modal.removeClass('inactivo');
+        $('.close').on('click', function (e) {
+            modal.addClass('inactivo');
+        });
+        console.log(post);
+        const {
+            description,
+            hashtag,
+            image_url,
+            title,
+            user,
+            username
+        } = post;
+        console.log(post);
+        $('.post--title').text(title);
+        $('.post--image').attr('src', `../images/${image_url}`);
+        $('.post--image').attr('alt', title);
+        $('.post--user').text(user);
+        $('.post--username').text(username.charAt(0).toUpperCase());
+        $('.post--hashtag').text(hashtag);
+        $('.post--description').text(description);
+    });
+};
+
+export default showmodal;
