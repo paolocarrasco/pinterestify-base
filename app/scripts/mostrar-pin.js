1dcb383fd7020c8c388c5d986e736932a35e524d
glisselisbeth <glisselisbeth@gmail.com>
2017-09-28 21:12:21 -0500

    add img

--- app/scripts/mostrar-pin.js
+++ app/scripts/mostrar-pin.js
@@ -1,12 +1,12 @@
 const mostrarPin = (pinContainer, pines) => {
     pinContainer.on('click', '.post', function(){
-      const id = $(this).find('p').data('id');
+      const id = $(this).attr('id');
       const modal = $('.modal');
       const [pin] = pines.filter(pin => pin.id == id);
       modal.removeClass('inactivo');
-      const {description, imagen_url, title, username, user, hashtag} = pin;
+      const {description, image_url, title, username, user, hashtag} = pin;
       modal.find('.post--title').text(title);
-      modal.find('.post--image').src('images/'+imagen_url);
+      modal.find('.post--image').attr('src', `images/${image_url}`);
       modal.find('.post--username').text(username);
       modal.find('.post--user').text(user);
       modal.find('.post--hashtag').text(`#${hashtag}`);
