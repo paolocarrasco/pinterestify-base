2ee827459b3d481e0ef44d68c779a3b1c0a22465
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-09-29 01:01:06 -0500

    modal-mostral detalle de pin seleccionado

--- app/scripts/mery-plugin.js
+++ app/scripts/mery-plugin.js
@@ -1,6 +1,8 @@
 import mostrarPines from './mostrar-pines.js';
+import mostrarPinDetalle from './mostrar-pin-detalle.js';
 
 $.fn.pinterestify = function(data){
   const [pines] = data;
   mostrarPines($(this), pines);
+  mostrarPinDetalle($(this), pines);
 };
---
+++ app/scripts/mostrar-pin-detalle.js
@@ -0,0 +1,19 @@
+const mostrarPinDetalle = (container, pines) => {
+  console.log("soy modal");
+  container.on('click', '.post', function(){
+    const id = $(this).attr('id');
+    const modal = $('.modal');
+    const [pin] = pines.filter(pin => pin.id == id);
+
+    modal.removeClass('inactivo');
+
+    const {title, description, user, image_url} = pin;
+
+    modal.find('.post--title').text(title);
+    modal.find('.post--image').attr('src', `images/${image_url}`);
+    modal.find('.post--image').attr('alt', title);
+    modal.find('.post--user').text(user);
+    modal.find('.post--description').text(description);
+  });
+};
+export default mostrarPinDetalle;
