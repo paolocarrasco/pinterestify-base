483bb1c77b50dcf972332874850f3982446a1dda
maia rojas <maia.rt.46@gmail.com>
2017-10-01 15:59:02 -0500

    Prueba close

--- app/scripts/resaltar-imagen.js
+++ app/scripts/resaltar-imagen.js
@@ -1,18 +1,18 @@
 const resaltarImagen = (imgContainer, imagenes) => {
 
-  console.log(imagenes);
-  
-  imgContainer.on('click', '.post', function(){
   imgContainer.on('click', '.post', function(){
 
-    console.log(imagenes);
      const id = $(this).attr('id');
      const modal = $('.modal');
+     const close =$('.close');
 
+     modal.on('click','.close', function(){
+      modal.addClass('inactivo');
+     })
      $.each(imagenes,function(i,e){
 
        const [imagen] = e.filter(imagen => imagen.id == id);
-       console.log(imagen);
+       
        modal.removeClass('inactivo');
        const {title,description ,user ,image_url,username , hashtag } = imagen;
 
